4d0d2b8f88e17f40efcf62e4e84b815a011520c9
@maltea <maltea@student.chalmers.se>
2026-03-19 22:33:41 +0100

    change title and icon on teh folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'We\'re Getting Married',
+  title: 'Engla & Jimmy',
   description: 'Join us for our wedding celebration',
 }
 
